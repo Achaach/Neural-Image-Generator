2b33795b2c4bbb4040ef9afcb1840130cac36641
Vivi <90078254+Achaach@users.noreply.github.com>
2023-02-27 08:22:33 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+unloader = transforms.ToPILImage() 
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  
+    image = image.squeeze(0)      
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001) 
+
+def gram_matrix(input):
+    a, b, c, d = input.size() 
+    features = input.view(a * b, c * d) 
+    gram = torch.mm(features, features.t())
+    return gram.div(a*b*c*d)
+
+
+class ContentLoss(nn.Module):
+
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+class StyleLoss(nn.Module):
+
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        return (img - self.mean) / self.std
+
